d63ed4a405f3dc477be6dcf50f0a869cf3fbab85
sonalipatel223 <sonalipatel07896@gmail.com>
2024-01-15 14:36:30 +0530

    save

---
+++ ORS_Project4/src/main/java/com/rays/pro4/Exception/RecordNotFoundException.java
@@ -0,0 +1,12 @@
+package com.rays.pro4.Exception;
+/**
+ * RecordNotFoundException thrown when a record not found occurred.
+ * 
+ * @author Sonali Patel
+ * 
+ */
+public class RecordNotFoundException extends Exception {
+	public RecordNotFoundException(String msg){
+		super(msg);
+	}
+}
